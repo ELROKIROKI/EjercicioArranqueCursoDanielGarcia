04ffc82b8e611dc4c736aa54c483567c47118742
daniels349959-dotcom <daniel.s349959@cesurformacion.com>
2026-09-22 08:20:28 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,104 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String[] tareas = new String[10];
+        boolean[] completadas = new boolean[10];
+        int contador = 0;
+
+        int opcion;
+        do {
+            System.out.println("\n--- GESTOR DE TAREAS ---");
+            System.out.println("1. Añadir tarea");
+            System.out.println("2. Ver tareas");
+            System.out.println("3. Completar tarea");
+            System.out.println("4. Eliminar tarea");
+            System.out.println("0. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            switch (opcion) {
+                case 0:
+                    System.out.println("Hasta luego.");
+                    break;
+                case 1:
+                    if (contador < tareas.length) {
+                        System.out.print("Escribe la tarea: ");
+                        String tarea = teclado.nextLine();
+                        tareas[contador] = tarea;
+                        completadas[contador] = false;
+                        ++contador;
+                        System.out.println("Tarea añadida.");
+                    } else {
+                        System.out.println("No puedes añadir más tareas.");
+                    }
+                    break;
+                case 2:
+                    if (contador == 0) {
+                        System.out.println("No hay tareas.");
+                    } else {
+                        System.out.println("\n--- TAREAS ---");
+
+                        for(int i = 0; i < contador; ++i) {
+                            String estado;
+                            if (completadas[i]) {
+                                estado = "[X]";
+                            } else {
+                                estado = "[ ]";
+                            }
+
+                            System.out.println(i + 1 + ". " + estado + " " + tareas[i]);
+                        }
+                    }
+                    break;
+                case 3:
+                    if (contador == 0) {
+                        System.out.println("No hay tareas para completar.");
+                    } else {
+                        System.out.print("Número de tarea completada: ");
+                        int numero = teclado.nextInt();
+                        teclado.nextLine();
+                        if (numero >= 1 && numero <= contador) {
+                            completadas[numero - 1] = true;
+                            System.out.println("Tarea completada.");
+                            continue;
+                        }
+
+                        System.out.println("Ese número no existe.");
+                    }
+                    break;
+                case 4:
+                    if (contador == 0) {
+                        System.out.println("No hay tareas para eliminar.");
+                    } else {
+                        System.out.print("Número de tarea que quieres eliminar: ");
+                        int numero = teclado.nextInt();
+                        teclado.nextLine();
+                        if (numero >= 1 && numero <= contador) {
+                            for(int i = numero - 1; i < contador - 1; ++i) {
+                                tareas[i] = tareas[i + 1];
+                                completadas[i] = completadas[i + 1];
+                            }
+
+                            tareas[contador - 1] = null;
+                            completadas[contador - 1] = false;
+                            --contador;
+                            System.out.println("Tarea eliminada.");
+                            continue;
+                        }
+
+                        System.out.println("Ese número no existe.");
+                    }
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while(opcion != 0);
+
+        teclado.close();
     }
 }
